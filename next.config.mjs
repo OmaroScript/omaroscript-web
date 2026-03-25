1a0666045732b52b6d2a502bc1e47a03fa48552a
Omar Hernandez <omar.hdz9619@gmail.com>
2026-03-24 19:49:15 -0600

    Added build prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-  // Any next config here
+  output: "export",
+  basePath: isProd ? "/omaroscript-web" : "",
+  assetPrefix: isProd ? "/omaroscript-web/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
